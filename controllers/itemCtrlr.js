cc561d5f957a167c514bf7987bbc05295e25ae15
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-01-14 18:23:34 -0600

    created first json item

---
+++ controllers/itemCtrlr.js
@@ -0,0 +1,26 @@
+// /////INDUCES for items
+// // PEOPLE INDEX ROUTE
+// const express = require('express');
+// const router = express.Router(); 
+// const Item = require('../models/item');
+
+
+
+// router.get("/item", async (req, res) => {
+//     try {
+//         // send all people
+//         res.json(await Item.find({}));
+//     } catch (error) {
+//         //send error
+//         res.status(400).json(error);
+//     }
+// });
+
+// //N
+// //D
+// //U
+// //C
+// //E
+// //S
+
+// module.exports = router; //
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
 require("dotenv").config()
 //.env
-const { PORT = 4000, MONGODB_URL } = process.env
+const { PORT = 3000, MONGODB_URI } = process.env
 //express
 const express = require("express")
 const app = express()
@@ -10,9 +10,11 @@ const mongoose = require("mongoose")
 //import middleware 
 const cors = require("cors")
 const morgan = require("morgan")
+const Item = require("./models/item")
+
 
 //DB CONNECTION 
-mongoose.connect(MONGODB_URL, {
+mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 })
@@ -20,6 +22,9 @@ mongoose.connection
     .on("open", () => console.log("You are connected to mongoose"))
     .on("close", () => console.log("You are disconnected from mongoose"))
     .on("error", (error) => console.log(error))
+
+mongoose.set('strictQuery', true);// only allows for strict informaiton reguarding our schema to be added 
+
     
 // MiddleWare
 ////////////////////////////////
@@ -28,11 +33,28 @@ app.use(morgan("dev")); // logging
 app.use(express.json()); // parse json bodies
 
 
+/////Routes 
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
 
-//////second test 
-/////INDUCDES TESSSTTT***** 
+// //i believe this will add controller milddle ware and handles request and sends a response 
+// const itemController = require("./controllers/itemCtrlr");
+// app.use('/item', itemController ); 
 
+////index controller 
+//i
+app.get("/item", async (req, res) => {
+    try {
+        // send all people
+        res.json(await Item.find({}));
+    } catch (error) {
+        //send error
+        res.status(400).json(error);
+    }
+});
 
+//c
 //Listener
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
 
