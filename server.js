b6ae9b4a4abc33314eaa844ef23745c37dc1bbe0
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-01-15 12:14:50 -0600

    fixed mongoose URL error, changed all to URI

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
 require("dotenv").config()
 //.env
-const { PORT = 3000, MONGODB_URI } = process.env
+const { PORT = 4000, MONGODB_URI } = process.env
 //express
 const express = require("express")
 const app = express()
@@ -22,66 +22,12 @@ mongoose.connection
     .on("open", () => console.log("You are connected to mongoose"))
     .on("close", () => console.log("You are disconnected from mongoose"))
     .on("error", (error) => console.log(error))
-
-mongoose.set('strictQuery', true);// only allows for strict informaiton reguarding our schema to be added 
-
     
 // MiddleWare
 ////////////////////////////////
 app.use(cors()); // to prevent cors errors, open access to all origins
 app.use(morgan("dev")); // logging
 app.use(express.json()); // parse json bodies
-
-//test route
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
-
-//item index route
-app.get("/items", async (req, res) => {
-    try {
-        // send all people
-        res.json(await Item.find({}))
-    } catch (error) {
-        //send error
-        res.status(400).json(error)
-    }
-})
-
-//item create route
-app.post("/items", async (req, res) => {
-    try {
-        // send all people
-        res.json(await Item.create(req.body))
-    } catch (error) {
-        //send error
-        res.status(400).json(error)
-    }
-})
-
-//item delete route
-app.delete("/items/:id", async (req, res) => {
-    try {
-        //send all items
-        res.json(await Item.findByIdAndRemove(req.params.id))
-    } catch (error) {
-        //send error
-        res.status(400).json(error)
-    }
-})
-
-//item update route
-app.put("/items/:id", async (req, res) => {
-    try {
-        //send all items
-        res.json(
-            await Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        )
-    } catch (error) {
-        //send error
-        res.status(400).json(error)
-    }
-})
 
 //test route
 app.get("/", (req, res) => {
