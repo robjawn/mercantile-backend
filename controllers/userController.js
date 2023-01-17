815d829428e8077ddc72637467c82404a94c1cc3
Robert Johnson <robljohn1998@gmail.com>
2023-01-17 17:30:41 -0500

    updating User Controller for heroku - should display data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
 //Dependencies
 const express = require('express')
-const userRouter = express.Router
+const userRouter = express.Router()
 const Item = require('../models/item');
 const Wanted = require('../models/wanted');
 const User = require('../models/user')
+
+userRouter.get("/", async (req, res) => {
+    try {
+        // send all wanted
+        res.json(await User.find({}))
+    } catch (error) {
+        //send error
+        res.status(400).json(error)
+    }
+})
 
 
 module.exports = userRouter
