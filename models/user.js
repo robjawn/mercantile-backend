105d9f39ea1cde80e340da3bfe055094b70a8c7b
Robert Johnson <robljohn1998@gmail.com>
2023-01-16 21:11:18 -0500

    wip: combines user and profile models

--- models/user.js
+++ models/user.js
@@ -6,7 +6,15 @@ const Schema = mongoose.Schema;
 const userSchema = Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  },  
+  name: { type: String, required: true},
+  about: { type: String},
+  location: { type: String },
+  avatar: { type: String},
+  items: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'Item' 
+    }]
+},  
     { timestamps: true }
         
     );
