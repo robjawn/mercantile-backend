65ba5e2f6aee94c5efb0924d58d584e4cba5e8a9
Robert Johnson <robljohn1998@gmail.com>
2023-01-19 12:36:29 -0500

    adds firebase authentication and administration

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 //Dependencies
 require("dotenv").config()
 //.env
-const { PORT = 4000, MONGODB_URL } = process.env
+const { PORT = 4000, MONGODB_URL, PRIVATE_KEY_ID, PRIVATE_KEY, CLIENT_ID } = process.env
 //express
 const express = require("express")
 const app = express()
+//firebase
+const admin = require('firebase-admin')
+const { getAuth } = require('firebase-admin/auth')
 //mongoose
 const mongoose = require("mongoose")
 //import middleware 
 const cors = require("cors")
 const morgan = require("morgan")
-//data & schemas
+//models
 const Item = require('./models/item')
 const Wanted = require('./models/wanted')
 const User = require('./models/user')
+
+admin.initializeApp({ 
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "m3rcantile-8f10b",
+        "private_key_id": PRIVATE_KEY_ID,
+        "private_key": PRIVATE_KEY.replace('\n', ''),
+        "client_email": "firebase-adminsdk-l7d6r@m3rcantile-8f10b.iam.gserviceaccount.com",
+        "client_id": CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-l7d6r%40m3rcantile-8f10b.iam.gserviceaccount.com" 
+    })
+})
 
 //DB CONNECTION 
 mongoose.connect(MONGODB_URL, {
@@ -30,6 +48,15 @@ mongoose.connection
 app.use(cors()); // to prevent cors errors, open access to all origins
 app.use(morgan("dev")); // logging
 app.use(express.json()); // parse json bodies
+// Authentication/Authorization 
+app.use(async function(req, res, next) {
+    const token = req.get('Authorization');
+    if(token) {
+        const user = await getAuth().verifyIdToken(token.replace('Bearer ', ''))
+        console.log(user)
+    }
+    next() // this function invokes the next middleware function in the middleware stack
+})
 
 /////routes////
 ////controllers for the routes///
