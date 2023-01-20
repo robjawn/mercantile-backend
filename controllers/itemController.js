6be98f6c6278364da5bf529999181ba17ce95b9a
Robert Johnson <robljohn1998@gmail.com>
2023-01-20 14:03:24 -0500

    wip on item post route, obtains email and name from token to be used in frontend

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -8,6 +8,23 @@ const User = require('../models/user')
 const admin = require('firebase-admin')
 const { getAuth } = require('firebase-admin/auth')
 
+itemRouter.use(async function(req, res, next) {
+    try{
+         const token = req.get('Authorization');
+         if(token) {
+             const user = await getAuth().verifyIdToken(token.replace('Bearer ', ''))
+             console.log(user)
+             req.user = user
+         } else {
+             req.user = null
+         }
+    } catch (error) {
+         req.user = null
+    }
+ 
+     next() // this function invokes the next middleware function in the middleware stack
+ })
+
 function isAuthenticated(req, res, next) {
     if(req.user) return next()
         res.status(401).json({ message: 'You must be logged in to do that'
@@ -29,7 +46,9 @@ itemRouter.get("/", async (req, res) => {
 itemRouter.post("/new", isAuthenticated, async (req, res) => {
     try {
         // attach uid
-        req.body.uid= req.user.uid
+        req.body.uid = req.user.uid
+        req.body.user = req.user.name
+        req.body.email = req.user.email
         ///send new item
         res.json(await Item.create(req.body))
     } catch (error) {
